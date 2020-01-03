1ffd4c1404202d8314eb51295ba8b1b9cbb257e1
GreRin <rigor1988@gmail.com>
2020-01-03 08:42:39 +0300

    init: start project youtube-api

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+	render() {
+		return (
+			<div className='search-bar ui segment'>
+
+			</div>
+		)
+	}
+}
+
+export default SearchBar;
