c81cadd610377b50f03f151fff4bdbad2aa1cbae
GreRin <rigor1988@gmail.com>
2020-01-03 08:44:27 +0300

    feat: add input form with onInputChange function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	state = {
+		myInput: ''
+	}
+
+	onInputChange = (event) => {
+		this.setState({ myInput: event.target.value })
+	}
 
 	render() {
 		return (
 			<div className='search-bar ui segment'>
-
+				<form className='ui form'>
+					<div className='field'>
+						<label>Video Search</label>
+						<input 
+							type='text'
+							value={this.state.myInput}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
 			</div>
 		)
 	}
